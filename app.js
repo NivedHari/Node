1010d90cef25f7efdb2df4e6f373cb26421b3a81
NivedHari <nivedhari44@gmail.com>
2024-02-14 15:35:13 +0530

    server

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+//   console.log(req);
+console.log("Nived");
+});
+
+server.listen(4000);
--- javascript.js
+++ javascript.js
@@ -1,11 +1,50 @@
-const ProductOfTwo = (a, b) => {
-  return a * b;
-};
+// // const ProductOfTwo = (a, b) => {
+// //   return a * b;
+// // };
 
-console.log(ProductOfTwo(2, 2));
+// // console.log(ProductOfTwo(2, 2));
 
-const student = {
-  name: "Max",
+// // const student = {
+// //   name: "Max",
 
-  age: 29,
-};
+// //   age: 29,
+// // };
+
+// // const fruits = ["apple", "oranges", " ", "mango", " ", "lemon"];
+// // const newFruits = fruits.map((fruit) => {
+// //   if (fruit === " ") {
+// //     fruit =  "empty string";
+// //   }
+// //   return fruit;
+// // });
+
+// // console.log(fruits);
+// // console.log(newFruits);
+
+// // setTimeout(()=>{
+// //     console.log("Tadaaa");
+// // },5000);
+// // console.log("Hellooooo");
+
+// const timer = (delay) => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         resolve();
+//       }, delay);
+//     });
+//   };
+  
+
+// const alphabet = async () => {
+//   console.log("a");
+
+//   console.log("b");
+
+//   await timer(console.log("c"), 3000);
+
+//   await timer(console.log("d"), 0);
+
+//   console.log("e");
+// };
+
+// alphabet();
