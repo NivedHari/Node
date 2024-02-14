7c2d7f3ecbca933376d99ff8b9e17de5cb060207
NivedHari <nivedhari44@gmail.com>
2024-02-14 13:14:46 +0530

    javascript refresher

--- first-app.js
+++ first-app.js
@@ -1,1 +1,3 @@
-console.log("Hello World");
+const fs = require('fs');
+
+fs.writeFileSync('hello.text','Hello from Node.js');
---
+++ javascript.js
@@ -0,0 +1,11 @@
+const ProductOfTwo = (a, b) => {
+  return a * b;
+};
+
+console.log(ProductOfTwo(2, 2));
+
+const student = {
+  name: "Max",
+
+  age: 29,
+};
